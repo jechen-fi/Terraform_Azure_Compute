105f3462654db02b8f2cab6a8ccd70d87f1db777
trtaylor-fi <81325977+trotaylor@users.noreply.github.com>
2021-08-13 12:56:19 -0500

    Update version.tf

--- Virtual_Machines/version.tf
+++ Virtual_Machines/version.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 2.64.0"
+      version = ">= 2.64.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "= 3.1.0"
+      version = ">= 3.1.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "= 3.1.0"
+      version = ">= 3.1.0"
     }
   }
   required_version = ">= 0.14.1, < 1.0.0"
