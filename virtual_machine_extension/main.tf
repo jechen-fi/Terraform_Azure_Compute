0b480a5bacd1ac2b59743a7df522484e3f6efe74
dstrat-FI <dstratman@fi.com>
2021-10-08 11:29:10 -0500

    updated method of accessing blob and moved most of settings into protected instead of public

--- virtual_machine_extension/main.tf
+++ virtual_machine_extension/main.tf
@@ -11,14 +11,19 @@ resource "azurerm_virtual_machine_extension" "vmextension" {
   auto_upgrade_minor_version = var.auto_upgrade
   settings                   = <<SETTINGS
     {  
-       "fileUris": ["${var.script_uri}"],   
-       "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File helloworld.ps1"
+       "timestamp": "100000000"
     }
 SETTINGS
   tags                       = var.tags
   protected_settings         = <<PROTECTED_SETTINGS
-    { 
-       "managedIdentity": { "clientId": "0dff075c-ebd9-4a3f-9976-5380ba73d67e" }
+    {
+       "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File helloworld.ps1",
+       "storageAccountName": "${var.script_storage_account}",
+       "storageAccountKey": "${var.script_storage_account_key}",
+       "fileUris": [
+         "${var.script_uri}"
+       ]
     }
 PROTECTED_SETTINGS
+# "managedIdentity": { "clientId": "0dff075c-ebd9-4a3f-9976-5380ba73d67e" },
 }
