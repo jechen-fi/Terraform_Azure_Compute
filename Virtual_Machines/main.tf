67db19749ffbfbbf3a49ecf180fd7b86fe4af0d6
dstrat-FI <dstratman@fi.com>
2021-11-02 12:44:58 -0500

    removed another extra char found

--- Virtual_Machines/main.tf
+++ Virtual_Machines/main.tf
@@ -114,7 +114,7 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   provision_vm_agent              = true
   allow_extension_operations      = true
   dedicated_host_id               = var.dedicated_host_id
-  availability_set_id             = var.enable_feature[var.enable_av_set]) ? concat(azurerm_availability_set.aset.id, [""]) : null
+  availability_set_id             = var.enable_feature[var.enable_av_set] ? concat(azurerm_availability_set.aset.id, [""]) : null
   encryption_at_host_enabled      = var.encryption_at_host_enabled
   tags                            = merge({ "ResourceName" = local.virtual_machine_name }, var.tags, )
   virtual_machine_scale_set_id    = var.vm_scale_set
