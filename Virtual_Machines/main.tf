ca45b1ffca3d7319551200b31589a48096654e6c
dstrat-FI <dstratman@fi.com>
2021-11-02 12:46:05 -0500

    Found another minor error

--- Virtual_Machines/main.tf
+++ Virtual_Machines/main.tf
@@ -231,7 +231,7 @@ resource "azurerm_windows_virtual_machine" "winvm" {
   allow_extension_operations = true
   dedicated_host_id          = var.dedicated_host_id
   license_type               = var.license_type
-  availability_set_id        = var.enable_feature[var.enable_av_set]) ? concat(azurerm_availability_set.aset.id, [""]) : null
+  availability_set_id        = var.enable_feature[var.enable_av_set] ? concat(azurerm_availability_set.aset.id, [""]) : null
   tags                       = merge({ "ResourceName" = local.virtual_machine_name }, var.tags, )
   dynamic "source_image_reference" {
     for_each = var.os_distribution_list[var.os_distribution][*]
