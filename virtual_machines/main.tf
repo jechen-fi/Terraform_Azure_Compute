41192cd2bb8cf22212a896f01ac4782894c6d3e0
dstratman-fi <81433279+dstratman-fi@users.noreply.github.com>
2022-07-06 16:31:03 -0500

    added zone param to windows vm and removed availability set option

--- virtual_machines/main.tf
+++ virtual_machines/main.tf
@@ -121,7 +121,7 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   provision_vm_agent              = true
   allow_extension_operations      = true
   dedicated_host_id               = var.dedicated_host_id
-  availability_set_id             = var.enable_feature[var.enable_av_set] ? element(concat(azurerm_availability_set.aset.*.id, [""]), count.index) : null
+  #availability_set_id             = var.enable_feature[var.enable_av_set] ? element(concat(azurerm_availability_set.aset.*.id, [""]), count.index) : null
   encryption_at_host_enabled      = var.encryption_at_host_enabled
   tags                            = merge({ "ResourceName" = local.virtual_machine_name }, var.tags, )
   virtual_machine_scale_set_id    = var.vm_scale_set
@@ -249,7 +249,8 @@ resource "azurerm_windows_virtual_machine" "winvm" {
   allow_extension_operations = true
   dedicated_host_id          = var.dedicated_host_id
   license_type               = var.license_type
-  availability_set_id        = var.enable_feature[var.enable_av_set] ? element(concat(azurerm_availability_set.aset.*.id, [""]), 0) : null
+  #availability_set_id        = var.enable_feature[var.enable_av_set] ? element(concat(azurerm_availability_set.aset.*.id, [""]), 0) : null
+  zone                       = var.zone
   tags                       = merge({ "ResourceName" = local.virtual_machine_name }, var.tags, )
   dynamic "source_image_reference" {
     for_each = var.os_distribution_list[var.os_distribution][*]
