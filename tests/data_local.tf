f5a5de423c4b28a5a9fefa61a2b4f64acfcd2f43
Pranay kumar reddy Akaram <p.akaram@fi.com>
2023-01-19 14:15:33 -0800

    test run

--- tests/data_local.tf
+++ tests/data_local.tf
@@ -15,11 +15,10 @@ data "azurerm_virtual_network" "vnet" {
 }
 
 data "azurerm_subnet" "snet" {
-  name                 ="a00000-app1-ctd"
+  name                 = "a00000-app1-ctd"
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   resource_group_name  = data.azurerm_resource_group.vnet_rg.name
 }
-
 data "azurerm_key_vault" "kv" {
   name                = "testkv098"
   resource_group_name = "a00000-namespace-ctd"
