e723ac80968569b728bce0debb3e151d46e3fe58
Pranay kumar reddy Akaram <p.akaram@fi.com>
2023-01-20 08:29:48 -0800

    Test run

--- tests/local.tf
+++ tests/local.tf
@@ -12,5 +12,5 @@ locals {
     applicationName = "VM & Extension module testing"
     environment     = "CTD"
   }
-  script_uri = "https://a00004sbxst001.blob.core.windows.net/artifacts/wrapper2.ps1,https://a00004sbxst001.blob.core.windows.net/artifacts/test1.ps1,https://a00004sbxst001.blob.core.windows.net/artifacts/test2.ps1"
+  script_uri = "https://a00004sbxst001.blob.core.windows.net/artifacts/wrapper2.ps1"
 }
