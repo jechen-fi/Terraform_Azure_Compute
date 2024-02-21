dbcfa40916a143772a31b28198f40d022266acf3
smccreary-fi <s.mccreary@fi.com>
2024-02-21 16:26:00 -0500

    ps1 filepath fix

--- virtual_machines/main.tf
+++ virtual_machines/main.tf
@@ -347,7 +347,7 @@ resource "azurerm_virtual_machine_extension" "install_ad" {
 
   protected_settings = <<SETTINGS
   {    
-    "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.domain_join_win.rendered)}')) | Out-File -filepath domain_join_win.ps1\" && powershell -ExecutionPolicy Unrestricted -File domain_join_win.ps1 -keyvault_domain_token ${data.template_file.domain_join_win.vars.keyvault_domain_token} -app_workload_group ${data.template_file.domain_join_win.vars.app_workload_group}"
+    "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.domain_join_win.rendered)}')) | Out-File -filepath ./modules/compute/virtual_machines/domain_join_win.ps1\" && powershell -ExecutionPolicy Unrestricted -File domain_join_win.ps1 -keyvault_domain_token ${data.template_file.domain_join_win.vars.keyvault_domain_token} -app_workload_group ${data.template_file.domain_join_win.vars.app_workload_group}"
   }
   SETTINGS
 }
